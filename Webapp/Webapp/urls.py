b4e79f39ee62369fa78b5bca4c6a1d901a29a337
aksh <sisodiyaaksh@gmail.com>
2020-09-25 19:05:23 +0530

    registrtion form HTML

--- Webapp/Webapp/urls.py
+++ Webapp/Webapp/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import index
+from .views import index, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('register/', register, name='register'),
 ]
--- Webapp/Webapp/views.py
+++ Webapp/Webapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'index.html',{'active_class':'home'})
+
+def register(request):
+    return render(request,'registration_form.html')
