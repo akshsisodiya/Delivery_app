ae520fb948ef8e942dc60fff8d045a172153d9b7
MANAV PUROHIT <manavjay305@gmail.com>
2020-12-13 15:43:19 +0530

    contribute kr diya hai  taanne mat dena behenchodd

--- Webapp/Webapp/urls.py
+++ Webapp/Webapp/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from .views import index
+from .views import index, about_us
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about-us/', about_us,name='about-us'),
     path('', index, name='index'),
     path('user/',include('user.urls')),
 ]
