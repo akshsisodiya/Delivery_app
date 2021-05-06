4ccaec8ec2c4daa239d5da085142596f6eccb91f
akshsisodiya <45943690+akshsisodiya@users.noreply.github.com>
2021-05-06 20:41:24 +0530

    Update settings.py

--- Webapp/Webapp/settings.py
+++ Webapp/Webapp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user',
+    'Webapp.user',
     'crispy_forms',    
     'django.contrib.sites',
     'allauth',
