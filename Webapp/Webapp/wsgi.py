5ccd6a2a25d69cca56a92abef8e35423c840af3d
akshsisodiya <45943690+akshsisodiya@users.noreply.github.com>
2021-05-06 20:35:18 +0530

    Update wsgi.py

--- Webapp/Webapp/wsgi.py
+++ Webapp/Webapp/wsgi.py
@@ -12,6 +12,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Webapp.settings')
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
